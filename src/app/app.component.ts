addff69ed92b73766f60a2514edb30ddb984c215
itsmebhaskar <itsmebhaskar1989@gmail.com>
2020-05-20 14:22:59 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,20 +7,32 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'FormControl Demo'
+  name = 'FormGroup Demo'
 
-//creating instance of FormControl with default value John
-  nameControl:FormControl=new FormControl('John');
+//creating instance of FormGroup with default values
+  formgrp:FormGroup=new FormGroup({
+  firstName:new FormControl(),
+  lastName:new FormControl(),
+  age:new FormControl()
+  });
 
   //Validation
-  // nameControl:FormControl=new FormControl('John',[Validators.required,Validators.maxLength(5)]);
+  // formgrp:FormGroup=new FormGroup({
+  // firstName:new FormControl('',[Validators.required]),
+  // lastName:new FormControl('',[Validators.required]),
+  // age:new FormControl(26,[Validators.required,Validators.min(20)])
+  // });
 
+
+get formgroupcontrols(){
+  return this.formgrp.controls;
+}
 //method to change the nameControl Value
 nameChange()
 {
-  alert(`Value before button click---------> ${this.nameControl.value}`)
-  this.nameControl.setValue('Jay');
-  alert(`Value after button click----------> ${this.nameControl.value}`)
+  alert(`Value before button click---------> ${JSON.stringify(this.formgrp.value)}`)
+  this.formgrp.setValue({firstName:'Bhaskar',lastName:'Reddy',age:30});
+  alert(`Value after button click----------> ${JSON.stringify(this.formgrp.value)}`)
 }
 
 
